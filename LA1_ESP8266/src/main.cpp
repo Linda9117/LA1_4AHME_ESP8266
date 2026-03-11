63a8e1e4282766ea3c2e68f6463393163c25dfab
Steinberger Linda <stelim22@htl-kaindorf.at>
2026-03-11 09:00:10 +0100

    11.03.2026 startzusatnd

--- LA1_ESP8266/src/main.cpp
+++ LA1_ESP8266/src/main.cpp
@@ -3,18 +3,22 @@
 #include "esp8266comport.h"
 #include "cli.h"
 
+
 CliComPort *uart0 = NULL;
 
 void setup() {
-  cliCreateComPort(&uart0, esp8266comportCreateSerialTx(0,76800));
+  cliCreateComPort(&uart0, esp8266comportCreateSerialTx(0, 76800));
   cliAddComPort(uart0);
 
   cliPrintPrompt(uart0, TXT_GREEN);
 }
 
 void loop() {
-  if(cliProcessRxData(uart0)){
-    cliPrintPrompt(uart0, TXT_CYAN);
+  if (cliHasInput(uart0)) {
+    cliReceiveByte(uart0);
   }
- 
+
+  if (cliProcessRxData(uart0)) {
+    cliPrintPrompt(uart0, TXT_GREEN);
+  }
 }
